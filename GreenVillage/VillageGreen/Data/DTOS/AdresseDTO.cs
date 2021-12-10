4851101cb03d3de47c2ffbbaac58b2e1f4d510eb
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-12-10 11:08:33 +0100

    10/12/2021 11:08

---
+++ GreenVillage/VillageGreen/Data/DTOS/AdresseDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillageGreen.Data.DTOS
+{
+    class AdresseDTO
+    {
+        public AdresseDTO()
+        {
+        }
+        public int IdAdresse { get; set; }
+        public string EmailAdresse { get; set; }
+        public string TelMobile { get; set; }
+        public string TelFixe { get; set; }
+        public string AdressePostale { get; set; }
+        public string Province { get; set; }
+        public string ComplementAdresse { get; set; }
+        public int IdVille { get; set; }
+    }
+}
