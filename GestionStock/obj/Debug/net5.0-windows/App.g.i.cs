3d6de646099dfa9d08171c2854396ccbf398b6bb
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-12-13 21:05:34 +0100

    Test Setup

--- GestionStock/obj/Debug/net5.0-windows/App.g.i.cs
+++ GestionStock/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace GestionStock {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace GestionStock {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
         public static void Main() {
             GestionStock.App app = new GestionStock.App();
             app.InitializeComponent();
