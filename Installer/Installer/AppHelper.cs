335e25ef8977edcfdb90c96691fc8197968ceb40
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-12-14 15:45:30 +0100

    Projet qui marche

---
+++ Installer/Installer/AppHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Installer
+{
+    public static class AppHelper
+    {
+        public static bool Save(string server, string database, string userbase, string password)
+        {
+            if (File.Exists(@"D:\Test.txt"))
+                File.Delete(@"D:\Test.txt");
+            File.WriteAllText(@"D:\Test.txt", $"Server={server}, Database={database},Userbase={userbase},Password={password}"); 
+            return true;
+        }
+    }
+}
---
+++ Installer/Installer/Installer1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Installer
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+            string server = Context.Parameters["Server"];
+            string database = Context.Parameters["Database"];
+            string username = Context.Parameters["Username"];
+            string password = Context.Parameters["Password"];
+            bool check = AppHelper.Save(server, database, username, password);
+            if(!check)
+            {
+                throw new Exception("Impossible de sauvegarder dans le fichier !!! ");
+            }
+        }
+    }
+}
